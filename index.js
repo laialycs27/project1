c78c144f776a61e22c9304ec759f5602b7ca3627
Laialy Abu awad <laialyabuawad@Laialys-MacBook-Air.local>
2025-06-09 10:06:34 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let count = 10;
+        
+        function increment() {
+            count+=10;
+            document.getElementById('count').innerText = count;
+        }
+        
+        function reset() {
+            count = 0;
+            document.getElementById('count').innerText = count;
+        }
